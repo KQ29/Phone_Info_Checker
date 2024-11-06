173486d29ad72f43aef7d6971d6092c46ce93d2f
KQ29 <ibragimovkamronbek2005@gmail.com>
2024-11-06 11:35:47 +0000

    Adding def get_phone_number

--- phone_number_checker.py
+++ phone_number_checker.py
@@ -2,3 +2,10 @@ import phonenumbers
 from phonenumbers import geocoder, carrier, timezone, number_type, is_possible_number
 import json 
 
+def get_phone_number():
+    phone_number = input("Enter phone number (with country code., +1234567890): ").strip()
+
+    if not phone_number:
+        print("Phone number cannot be empty.")
+        return None
+    return phone_number
